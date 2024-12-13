170aa16d066008e56187417b407560c102409fb8
Nikita Akimov <korchiy@yandex.ru>
2024-12-13 14:01:39 +0300

    update

--- arc_3.py
+++ arc_3.py
@@ -39,12 +39,11 @@ class Arc3:
             bpy.ops.object.mode_set(mode='OBJECT')
         # switch to vertex selection mode
         # context.tool_settings.mesh_select_mode = (True, False, False)
+        obj_world_matrix = obj.matrix_world.copy()
         bm = bmesh.new()
         bm.from_mesh(obj.data)
         bm.verts.ensure_lookup_table()
         bm.edges.ensure_lookup_table()
-
-        obj_world_matrix = obj.matrix_world.copy()
 
         # get 3 starting vertices
         v0 = v1 = v2 = None
@@ -55,52 +54,39 @@ class Arc3:
                 v2_edge = next((edge for edge in v1.link_edges if edge != v0.link_edges[0]), None)
                 v2 = v2_edge.other_vert(v1) if v2_edge else None
 
-        # points_normal = (v0.co - v1.co).cross(v2.co - v1.co)
-        points_normal = (v1.co * obj_world_matrix - v0.co * obj_world_matrix).cross(v1.co * obj_world_matrix - v2.co * obj_world_matrix)
-        points_normal.normalize()
 
         if v0 and v1 and v2:
 
-            # https://github.com/sergarrido/random/blob/master/circle3d/circle3d.cpp#L63
-            # p1 = v0.co
-            # p2 = v1.co
-            # p3 = v2.co
-            p1 = v0.co * obj_world_matrix
-            p2 = v1.co * obj_world_matrix
-            p3 = v2.co * obj_world_matrix
+            # points coordinates in global space
+            v0gco = obj_world_matrix * v0.co
+            v1gco = obj_world_matrix * v1.co
+            v2gco = obj_world_matrix * v2.co
 
-            v1 = p2 - p1
-            v2 = p3 - p1
+            # get circle data
+            circle_center, circle_radius = cls._circle_by_3_points(
+                v0=v0gco,
+                v1=v1gco,
+                v2=v2gco
+            )
 
-            v1v1 = v1.dot(v1)
-            v2v2 = v2.dot(v2)
-            v1v2 = v1.dot(v2)
+            # get normal to plane formed by all this 3 points
+            normal = (v1gco - v0gco).cross(v1gco - v2gco)
+            normal.normalize()
 
-            base = 0.5 / (v1v1 * v2v2 - v1v2 * v1v2)
-            k1 = base * v2v2 * (v1v1 - v1v2)
-            k2 = base * v1v1 * (v2v2 - v1v2)
-
-            c = p1 + v1 * k1 + v2 * k2
-            print('c', c)
-            bpy.context.scene.cursor_location = c
-            radius = (c - p1).length
-            print('radius', radius)
+            bpy.context.scene.cursor_location = circle_center
 
             # for i in range(points):
             #     bm.verts.new(v)
 
-            bpy.ops.mesh.primitive_circle_add(radius=radius, location=c)
-            # bpy.ops.mesh.primitive_circle_add(radius=radius, location=(0, 0, 0))
+            bpy.ops.mesh.primitive_circle_add(radius=circle_radius, location=circle_center)
 
             # transform matrix from normal (0, 0, 1) to normal of the 3 points plane
-
-            m = cls._transform_from_normal_to_normal(
+            m = cls._transform_matrix_from_normal_to_normal(
                 src_normal=Vector((0.0, 0.0, 1.0)),
-                dest_normal=points_normal
+                dest_normal=normal
             )
-            print(m)
-
-            print(bpy.context.object)
+            print('m', m)
+            print('q', m.to_quaternion())
 
             bpy.context.object.matrix_world *= m
 
@@ -111,66 +97,38 @@ class Arc3:
         # bpy.ops.object.mode_set(mode=mode)
 
     @staticmethod
-    def _transform_from_normal_to_normal(src_normal, dest_normal):
+    def _circle_by_3_points(v0: Vector, v1: Vector, v2: Vector):
+        # get circle from 3 points (coordinates in global space)
+        #   return circle center coordinates and radius length
+        vv1 = v1 - v0
+        vv2 = v2 - v0
+        v1v1 = vv1.dot(vv1)
+        v2v2 = vv2.dot(vv2)
+        v1v2 = vv1.dot(vv2)
+        base = 0.5 / (v1v1 * v2v2 - v1v2 * v1v2)
+        k1 = base * v2v2 * (v1v1 - v1v2)
+        k2 = base * v1v1 * (v2v2 - v1v2)
+        center = v0 + vv1 * k1 + vv2 * k2
+        radius = (center - v0).length
+        return center, radius
 
-        z = dest_normal
-        d = src_normal
-
-        # v = cross(z, d);
-        v = z.cross(d)
-        # c = dot(z, d);
-        c = z.dot(d)
+    @staticmethod
+    def _transform_matrix_from_normal_to_normal(src_normal: Vector, dest_normal: Vector):
+        # get transform matrix from one normal to another
+        v = dest_normal.cross(src_normal)
+        c = dest_normal.dot(src_normal)
         k = 1.0 / (1.0 + c)
-
-        m = Matrix(
+        transform_matrix = Matrix(
             ((v.x * v.x * k + c, v.y * v.x * k - v.z, v.z * v.x * k + v.y),
              (v.x * v.y * k + v.z, v.y * v.y * k + c, v.z * v.y * k - v.x),
              (v.x * v.z * k - v.y, v.y * v.z * k + v.x, v.z * v.z * k + c))
         )
-        m = m.to_4x4()
-
-        m.transpose()
-        
-        return m
-
-        # https://stackoverflow.com/questions/52189123/calculate-rotation-matrix-to-transform-one-vector-to-another
-
-        # # V = normalize(cross(src_normal, dest_normal));
-        # V = src_normal.cross(dest_normal)
-        # V.normalize()
-        #
-        # # phi = acos(dot(src_normal, dest_normal));
-        # phi = acos(src_normal.dot(dest_normal))
-        #
-        # # rcos = cos(phi);
-        # rcos = cos(phi)
-        #
-        # # rsin = sin(phi);
-        # rsin = sin(phi)
-        #
-        # M = Matrix()
-        #
-        # M[0][0] = rcos + V.x * V.x * (1.0 - rcos)
-        # M[1][0] = V.z * rsin + V.y * V.x * (1.0 - rcos)
-        # M[2][0] = -V.y * rsin + V.z * V.x * (1.0 - rcos)
-        # M[3][0] = 0.0
-        #
-        # M[0][1] = -V.z * rsin + V.x * V.y * (1.0 - rcos)
-        # M[1][1] = rcos + V.y * V.y * (1.0 - rcos)
-        # M[2][1] = -V.x * rsin + V.z * V.y * (1.0 - rcos)
-        # M[3][1] = 0.0
-        #
-        # M[0][2] = V.y * rsin + V.x * V.z * (1.0 - rcos)
-        # M[1][2] = -V.x * rsin + V.y * V.z * (1.0 - rcos)
-        # M[2][2] = rcos + V.z * V.z * (1.0 - rcos)
-        # M[3][2] = 0.0
-        #
-        # M[0][3] = 0.0
-        # M[1][3] = 0.0
-        # M[2][3] = 0.0
-        # M[3][3] = 1.0
-        #
-        # return M
+        # convert to 4x4 matrix for Blender transformations
+        transform_matrix = transform_matrix.to_4x4()
+        # convert from left-hand orientation to Blender right-hand orientation
+        transform_matrix.transpose()
+        # return final transformation matrix
+        return transform_matrix
 
     @staticmethod
     def ui(layout, context):
